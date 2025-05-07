1b86e65df29b6f94420d769754f842c7d13d53b7
azim-mia <mdazimmia01980@gmail.com>
2025-05-07 12:15:28 +0600

    add class

---
+++ class_6/index.js
@@ -0,0 +1,15 @@
+/*time complexity and space complexity simple discutions*/
+const uniqucName = (arr)=>{
+  let chackDubbleName = [];
+  for(let i = 0; i < arr.length; i++){ //time complexity big 0(n2)
+    let els = arr[i];
+    if(!chackDubbleName.includes(els)){
+      chackDubbleName.push(els)
+    }
+  }
+  return chackDubbleName; // space complexity big o(n)
+}
+let nameArray = ['azim', 'karim','azim','millon','piyas','azim'];
+console.log(uniqucName(nameArray));
+//time complexity big 0(n2)
+// space complexity big o(n)
